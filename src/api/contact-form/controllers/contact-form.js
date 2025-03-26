44a86cd96e3ca4160e4063554b2563c194d5dd1f
SelminLekovic <selmin@origin3agency.com>
2025-03-26 13:42:05 +0100

    new

--- src/api/contact-form/controllers/contact-form.js
+++ src/api/contact-form/controllers/contact-form.js
@@ -27,7 +27,7 @@ module.exports = {
 
 
       let emailText =
-        `Full Name: ${name} ${surname}\n` +
+        `Full Name: ${name}\n` +
         `Email: ${email}\n` +
         `${message}`;
 
@@ -45,45 +45,5 @@ module.exports = {
       ctx.send({ error: 'Failed to send email', details: error.message });
     }
   },
-  async sendResult(ctx) {
-    try {
-      const data = ctx.request.body; // FIXED: Extract request body correctly
 
-      console.log("Received Data:",  ctx.request);
-
-
-      const transporter = nodemailer.createTransport({
-        service: "gmail",
-        auth: {
-          user: process.env.SMTP_EMAIL,
-          pass: process.env.SMTP_TOKEN,
-        },
-        secure: true, // FIXED: Ensure secure connection for Gmail
-        debug: true,
-      });
-
-      const emailMessage = {
-        from: "selmin@origin3agency.com",
-        to: "selminlekovic@gmail.com",
-        subject: "Insurance Calculation Result",
-        text: `
-          First Name: ${data.first_name}
-          Last Name: ${data.last_name}
-          Birthday: ${data.birthday}
-          Email: ${data.email}
-          Gender: ${data.sex}
-          Insurance Period: ${data.insurance} years
-          Insurance Price: ${data.insurance_price} EUR
-          Monthly Premium: ${data.monthly_premium} EUR
-        `,
-      };
-
-      await transporter.sendMail(emailMessage);
-
-      ctx.send({ message: "Email sent successfully!" });
-    } catch (error) {
-      console.error("Email sending failed:", error);
-      ctx.send({ error: "Failed to send email", details: error.message });
-    }
-  },
 };
